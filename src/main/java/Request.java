513661494189c780d39ec3f9808cf0bc8ba247fa
Neil.Sarkar <ansarkar@outlook.com>
2017-03-30 01:32:17 +1100

    some changes
    not much

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -22,24 +22,24 @@ public class Request {
 
     public String makeRequest(){
         StringBuilder result = new StringBuilder();
-        String query = "PREFIX\tw3:\t<http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-                "PREFIX\tfe:\t<http://adage.cse.unsw.edu.au/ontology/financial-events#>\n" +
-                "PREFIX\tins:\t<http://adage.cse.unsw.edu.au/resource/financial-events#>\n" +
-                "PREFIX\txs:\t<http://www.w3.org/2001/XMLSchema#>\n" +
-                "SELECT\t?s\t?time\t?headline\t?newsBody\t\n" +
-                "WHERE\t{\n" +
-                "?s\tw3:type\tfe:TRTHNewsEvent.\n" +
-                "?s\tfe:relatedRIC\t?ric.\n" +
-                "?s\tfe:timeStamp\t?t.\n" +
-                "?t\tfe:startTime\t?time.\n" +
-                "?s\tfe:newsText\t?newsBody.\n" +
-                "?s\tfe:headLine\t?headline.\n" +
-                "?s\tfe:topicCode\t?topicCode.\n" +
-                "?s\tfe:languageOfNews\t\"en\".\n" +
-                "\t\tFILTER\t(?ric\t=\tins:ANZ.AX)\n" + //instrument
-                "\t\tFILTER\t(?topicCode\t=\t\"N2:ECB\")\n" + //topic code
-                "\t\tFILTER(xs:dateTime(?time)\t>\t\"2015-10-01T08:45:10.295Z\"^^xs:dateTime\t&&\txs:dateTime(?time)\t<=\t\n" + //start date
-                "\"2015-11-01T19:37:12.193Z\"^^xs:dateTime)\n" + //enddate
+        String query = "PREFIX w3: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX fe: <http://adage.cse.unsw.edu.au/ontology/financial-events#>\n" +
+                "PREFIX ins: <http://adage.cse.unsw.edu.au/resource/financial-events#>\n" +
+                "PREFIX xs: <http://www.w3.org/2001/XMLSchema#>\n" +
+                "SELECT ?s ?time ?headline ?newsBody \n" +
+                "WHERE {\n" +
+                "?s w3:type fe:TRTHNewsEvent.\n" +
+                "?s fe:relatedRIC ?ric.\n" +
+                "?s fe:timeStamp ?t.\n" +
+                "?t fe:startTime ?time.\n" +
+                "?s fe:newsText ?newsBody.\n" +
+                "?s fe:headLine ?headline.\n" +
+                "?s fe:topicCode ?topicCode.\n" +
+                "?s fe:languageOfNews \"en\".\n" +
+                "  FILTER (?ric = ins:RIC_"+instrumentID+")\n" + //instrument
+                "  FILTER (?topicCode = \"N2:"+topicCode+"\")\n" + //topic code
+                "  FILTER(xs:dateTime(?time) > "+startDate+"^^xs:dateTime && xs:dateTime(?time) <= \n" + //start date
+                ""+endDate+"^^xs:dateTime)\n" + //enddate
                 "}";
 
         Authenticator.setDefault (new Authenticator() {
@@ -54,6 +54,7 @@ public class Request {
             URL obj = new URL(url);
             HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
             conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/sparql-results+json");
             BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String line;
             while ((line = rd.readLine()) != null) {
